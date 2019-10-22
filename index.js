12cf755102477a8e34233c09e00bff607f678de0
Alireza Rezania <alireza.rzna@gmail.com>
2019-10-22 21:05:53 +0330

    [web] load icon fonts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {AppRegistry, Platform} from 'react-native';
 import 'react-native-gesture-handler';
 import {useScreens} from 'react-native-screens';
 import App from './src/App';
+
 const appName = 'Whatslack';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -14,7 +15,9 @@ useScreens();
 
 AppRegistry.registerComponent(appName, () => App);
 
-Platform.OS === 'web' &&
+if (Platform.OS === 'web') {
   AppRegistry.runApplication(appName, {
     rootTag: document.getElementById('root'),
   });
+  require('./web/load-fonts').default();
+}
