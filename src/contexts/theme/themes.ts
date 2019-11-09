ce0b7574f5fd5a39dedc2d2dfba5edac1a95e2fc
Alireza Rezania <alireza.rzna@gmail.com>
2019-11-09 18:41:29 +0330

    add amoled theme

--- src/contexts/theme/themes.ts
+++ src/contexts/theme/themes.ts
@@ -5,3 +5,5 @@ export const darkBlueTheme = createThemeFromColor('#242B38', 'darkBlueTheme', 'D
 export const lightWhite = createThemeFromColor('#FFFFFF', 'lightWhite', 'Light White');
 
 export const darkBlack = createThemeFromColor('#282828', 'darkBlack', 'Dark Black');
+
+export const amoledBlack = createThemeFromColor('#000', 'amoledBlack', 'Amoled Black');
