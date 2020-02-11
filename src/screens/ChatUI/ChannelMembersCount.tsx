be768c3ae4678f9dd0ba1e630686de89ff333f95
Alireza Rezania <alireza.rzna@gmail.com>
2020-02-11 15:15:09 +0330

    refactor ChatMembersCount

--- src/screens/ChatUI/ChannelMembersCount.tsx
+++ src/screens/ChatUI/ChannelMembersCount.tsx
@@ -1,15 +1,26 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
+import {useSelector} from 'react-redux';
+import {Text, StyleSheet, TextStyle} from 'react-native';
+
 import {RootState} from '../../reducers';
-import { connect } from 'react-redux';
-import { Text, StyleSheet, TextStyle } from 'react-native';
 import px from '../../utils/normalizePixel';
 
-type Props = ReturnType<typeof mapStateToProps> & {
-  style?: TextStyle
+interface Props {
+  style?: TextStyle;
+  chatId: string;
 }
 
-const ChatMembersCount: FC<Props> = ({fullLoad, chat, style}) => {
-  return <Text style={[styles.membersCount, style]}>{fullLoad?.loading? '...' : fullLoad?.loaded? `${chat?.num_members}`: ''} members</Text>
+const ChatMembersCount: FC<Props> = ({chatId, style}) => {
+  const {chat, fullLoad} = useSelector((state: RootState) => ({
+    chat: state.entities.chats.byId[chatId],
+    fullLoad: state.chats.fullLoad[chatId],
+  }));
+
+  return (
+    <Text style={[styles.membersCount, style]}>
+      {fullLoad?.loading ? '...' : fullLoad?.loaded ? `${chat?.num_members}` : ''} members
+    </Text>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -18,11 +29,6 @@ const styles = StyleSheet.create({
     marginTop: px(2.5),
     fontSize: px(13.5),
   },
-})
-
-const mapStateToProps = (state: RootState, ownProps) => ({
-  chat: state.entities.chats.byId[ownProps.chatId],
-  fullLoad: state.chats.fullLoad[ownProps.chatId],
 });
 
-export default connect(mapStateToProps)(ChatMembersCount) ;
+export default ChatMembersCount;
